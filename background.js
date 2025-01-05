d85bbb6f10e9be2f805ffce6a59f9b9c857516c6
rishabhrajphx <rishabhnewmob@gmail.com>
2025-01-04 22:21:54 -0700

    background.js added.

---
+++ background.js
@@ -0,0 +1,41 @@
+const TWITTER_URL = "https://twitter.com/*";
+const LEETCODE_URL = "https://leetcode.com/";
+
+let twitterTabId = null;
+let twitterStartTime = null;
+
+// Listener for tab updates
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url && tab.url.match(/^https?:\/\/(www\.)?twitter\.com\/.*/)) {
+    if (twitterTabId !== tabId) {
+      twitterTabId = tabId;
+      twitterStartTime = Date.now();
+
+      // Set an alarm for 30 minutes
+      chrome.alarms.create("twitterTimeout", { delayInMinutes: 30 });
+    }
+  }
+});
+
+// Listener for tab removal
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+  if (tabId === twitterTabId) {
+    twitterTabId = null;
+    twitterStartTime = null;
+    chrome.alarms.clear("twitterTimeout");
+  }
+});
+
+// Listener for alarms
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "twitterTimeout" && twitterTabId) {
+    // Close Twitter tab
+    chrome.tabs.remove(twitterTabId, () => {
+      // Open LeetCode
+      chrome.tabs.create({ url: LEETCODE_URL });
+      // Reset variables
+      twitterTabId = null;
+      twitterStartTime = null;
+    });
+  }
+});
