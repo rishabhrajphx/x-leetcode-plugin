33b0f2dc33c7c8434aa45b9819611563b4e4eb71
rishabhrajphx <rishabhnewmob@gmail.com>
2025-01-04 22:53:29 -0700

    background.js updated

--- background.js
+++ background.js
@@ -1,18 +1,23 @@
 const TWITTER_URL = "https://twitter.com/*";
 const LEETCODE_URL = "https://leetcode.com/";
 
+// Update the Twitter URL pattern to match more precisely
+const TWITTER_PATTERN = /^https:\/\/([\w-]+\.)?twitter\.com/;
+
 let twitterTabId = null;
 let twitterStartTime = null;
 
 // Listener for tab updates
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (tab.url && tab.url.match(/^https?:\/\/(www\.)?twitter\.com\/.*/)) {
+  // Only proceed if the URL has changed and the tab is complete
+  if (changeInfo.status === 'complete' && tab.url && TWITTER_PATTERN.test(tab.url)) {
     if (twitterTabId !== tabId) {
       twitterTabId = tabId;
       twitterStartTime = Date.now();
 
-      // Set an alarm for 30 minutes
-      chrome.alarms.create("twitterTimeout", { delayInMinutes: 1 });
+      // Set an alarm for 30 minutes (changed from 1 minute)
+      chrome.alarms.create("twitterTimeout", { delayInMinutes: 30 });
+      console.log("Timer started for Twitter");
     }
   }
 });
